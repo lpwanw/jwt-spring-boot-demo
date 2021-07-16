eac5a2e529d77ce239ed5523451e570a68f14abf
Lê Phương Tây <41092293+lpwanw@users.noreply.github.com>
2021-07-16 20:39:58 +0700

    update

--- src/main/java/com/lpwanw/AppServer/Calculator/Cal2.java
+++
@@ -1,5 +0,0 @@
-package com.lpwanw.AppServer.Calculator;
-
-public interface Cal2 {
-    public int tinhtong(int a,int b);
-}
--- src/main/java/com/lpwanw/AppServer/controller/HelloController.java
+++ src/main/java/com/lpwanw/AppServer/controller/HelloController.java
@@ -1,7 +1,4 @@
 package com.lpwanw.AppServer.controller;
-
-import com.lpwanw.AppServer.Calculator.Cal;
-import com.lpwanw.AppServer.Calculator.Cal2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
@@ -11,10 +8,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-	@Autowired
-	private Cal calculator;
-	@Autowired
-	private Cal2 calculator2;
 	@RequestMapping(value = {"/hello"})
 	public String sayHello(Model model) {
 		String name = "Tây";
